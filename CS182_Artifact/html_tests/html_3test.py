eadefc7fc53052da417a8da60da33d903e3aa668
lb-rodrigo <77036781+lb-rodrigo@users.noreply.github.com>
2022-12-09 20:21:37 -0800

    Add files via upload
    HTML parser fuzz

---
+++ CS182_Artifact/html_tests/html_3test.py
@@ -0,0 +1,19 @@
+import atheris
+import sys
+
+with atheris.instrument_imports():
+    from bs4 import BeautifulSoup as bs
+
+@atheris.instrument_func
+def TestOneInput(data):
+
+    fdp = atheris.FuzzedDataProvider(data)
+    data = fdp.ConsumeString(len(data))
+
+    # p = fdp.ConsumeString(len(data))
+
+    soup = bs(data, features ="html.parser")
+    return soup.find_all(id=data)
+    
+atheris.Setup(sys.argv, TestOneInput)
+atheris.Fuzz()
---
+++ CS182_Artifact/html_tests/html_test.py
@@ -0,0 +1,20 @@
+import atheris
+import sys
+
+with atheris.instrument_imports():
+    from bs4 import BeautifulSoup as bs
+
+@atheris.instrument_func
+def TestOneInput(data):
+
+    fdp = atheris.FuzzedDataProvider(data)
+    data = fdp.ConsumeString(len(data))
+
+    soup = bs(data, features ="html.parser")
+    return soup.find_all(data)
+    
+atheris.Setup(sys.argv, TestOneInput)
+atheris.Fuzz()
+
+
+
